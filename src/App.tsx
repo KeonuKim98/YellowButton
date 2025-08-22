52c8c8bff213023983a8f64c901057b9302d42e6
KeonuKim98 <maxman0107@naver.com>
2025-08-23 03:47:16 +0900

    [feat] 예약 페이지 datePicker 추가 - LocalizationProvide는 app.tsx 최상단에 추가 - 폼 예약날짜 추가
    todo : 코드 리팩토링 필요

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,30 @@ import { QueryClientProvider } from '@tanstack/react-query';
 import { queryClient } from './queryClient';
 import { RouterProvider } from 'react-router-dom';
 import { MainRouter } from './router/MainRouter';
+import { LocalizationProvider } from '@mui/x-date-pickers';
+import { koKR } from '@mui/x-date-pickers/locales';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 
 const App = () => {
   // const [count, setCount] = useState(0);
 
   return (
-    <QueryClientProvider client={queryClient}>
-      <RouterProvider router={MainRouter}></RouterProvider>
-    </QueryClientProvider>
+    <LocalizationProvider
+      dateAdapter={AdapterDayjs}
+      localeText={
+        koKR.components.MuiLocalizationProvider.defaultProps.localeText
+      }
+      // adapterLocale={'zh-cn'}
+      // dateFormats={
+      //   {
+      //     monthAndYear: 'YYYY년 MM월',
+      //   } as any
+      // }
+    >
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={MainRouter}></RouterProvider>
+      </QueryClientProvider>
+    </LocalizationProvider>
   );
 };
 
--- src/pages/Reservation/ReservationHome.tsx
+++ src/pages/Reservation/ReservationHome.tsx
@@ -1,4 +1,6 @@
-import { Stack } from '@mui/material';
+import { Stack, TextField, Typography } from '@mui/material';
+import { TextFieldSx, TextInputProps, TitleSx } from './ReservationFormStyles';
+import { DatePicker, DateTimePicker } from '@mui/x-date-pickers';
 
 export const ReservationHome = () => {
   return (
@@ -6,8 +8,53 @@ export const ReservationHome = () => {
       sx={{
         height: '100%',
         width: '100%',
-        backgroundColor: '#000290ff',
+        backgroundColor: '#ffffffff',
+        padding: '10px',
       }}
-    ></Stack>
+    >
+      <Stack
+        sx={{
+          height: '100%',
+          width: '100%',
+          backgroundColor: '#ffffffff',
+          // padding: '10px',
+        }}
+      >
+        <Typography
+          sx={{
+            width: '100%',
+            fontSize: '20px',
+            marginBottom: '50px',
+            alignContent: 'center',
+          }}
+        >
+          예약하기
+        </Typography>
+        <Stack
+          sx={{
+            height: '100%',
+          }}
+          gap="20px"
+        >
+          <Stack direction="row" gap="10px">
+            <Typography sx={TitleSx}>이름</Typography>
+            <TextField
+              inputProps={TextInputProps}
+              sx={{ ...TextFieldSx, width: '100%' }}
+            />
+          </Stack>
+          <Stack direction="row" gap="10px">
+            <Typography sx={TitleSx}>예약날짜</Typography>
+            <DateTimePicker
+              sx={{ width: '100%', borderRadius: '100px' }}
+              format="YYYY년 MM월 DD일 HH:mm"
+            ></DateTimePicker>
+          </Stack>
+          <Stack direction="row" gap="10px">
+            <Typography sx={TitleSx}>인원수</Typography>
+          </Stack>
+        </Stack>
+      </Stack>
+    </Stack>
   );
 };
